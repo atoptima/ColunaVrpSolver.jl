d49a63023d6261773a0cc44c00d7afb4f9d00274
Artur Alves Pessoa <artur.a.pessoa@gmail.com>
2022-10-01 10:08:13 -0300

    Testing with a larger instance

--- test/integration/medsize.jl
+++ test/integration/medsize.jl
@@ -3,8 +3,6 @@ include("../../$appfolder/src/run.jl")
 
 function run_mediumsize_instances_tests()
     @testset "Solving selected medium-size CVRP instance" begin
-        main(["../$appfolder/data/F/F-n45-k4.vrp", "-m", "4", "-M", "4", "-u", "725"])
-        # main(["../$appfolder/data/E/E-n51-k5.vrp", "-m", "5", "-M", "5", "-u", "522"])
-        # main(["../$appfolder/data/A/A-n37-k6.vrp", "-m", "6", "-M", "6", "-u", "950"])
+        main(["../$appfolder/data/F/F-n72-k4.vrp", "-m", "4", "-M", "4", "-u", "238"])
     end
 end
